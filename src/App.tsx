16ba5d398eb23f840d32f8eeec6fd684a70f5a59
MoepyaCode <kagisomoepya19@gmail.com>
2024-08-18 23:58:56 +0200

    🎉 Project init huddle-landing-page-with-a-single-introductory-section

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Assets } from './assets'
+import { Container, Main, Wrapper } from './components'
+
+export default function App() {
+
+  return (
+    <Main className='flex flex-col gap-[65px] min-w-full bg-[#684BB1] bg-[url(/src/assets/bg-mobile.svg)] bg-no-repeat px-[36px] py-[40px]'>
+      hello world
+    </Main>
+  )
+}
---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import bgDesktop from './bg-desktop.svg';
+import bgMobile from './bg-mobile.svg';
+import illustrationMockups from './illustration-mockups.svg';
+import logo from './logo.svg';
+
+export const Assets = {
+    bgDesktop,
+    bgMobile,
+    illustrationMockups,
+    logo,
+}
